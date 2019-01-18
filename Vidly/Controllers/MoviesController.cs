e2f5a28d29705fa093d10cdc57df27432481f193
Roberto Minghi <robertominghi@msn.com>
2019-01-18 13:48:47 +0000

    project set up properly

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private static List<Movie> _movies = new List<Movie>
+        {
+            new Movie { Name = "Shrek", Id = 1 },
+            new Movie { Name = "Wall-e", Id = 2 }
+        };
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Person1" },
+                new Customer { Name = "Person2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        // GET: Movies/Edit/{int}
+        public ActionResult Edit(int id)
+        {
+            return Content("Id =" + id);
+        }
+
+        // GET: Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return View(_movies); ;
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
