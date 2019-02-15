02172cb488fa32c21a354246bc880f8139339d96
Roberto Minghi <robertominghi@msn.com>
2019-02-15 16:29:22 +0000

    rest api

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dddd, MMMM dd, yyyy}")]
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dddd, MMMM dd, yyyy}")]
+        public DateTime? DateAdded { get; set; }
+
+        [Display(Name = "Number In Stock")]
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+    }
+}
